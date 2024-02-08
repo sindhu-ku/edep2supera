e02db42ad4c4a85caec95ca22bffbe9ea44d49b3
Zachary Hulcher <12894838+zhulcher@users.noreply.github.com>
2024-02-07 17:49:00 -0800

    adding final momentum and osx fixes

--- python/edep2supera/__init__.py
+++ python/edep2supera/__init__.py
@@ -2,12 +2,22 @@ import ROOT,os
 
 # Load supera library by hand as it's not bound to python through PyROOT
 import supera
-ROOT.gSystem.Load(os.path.join(supera.get_lib_dir(),'libsupera.so'))
+if os.path.isfile(os.path.join(supera.get_lib_dir(),'libsupera.so')):
+    ROOT.gSystem.Load(os.path.join(supera.get_lib_dir(),'libsupera.so'))
+elif os.path.isfile(os.path.join(supera.get_lib_dir(),'libsupera.dylib')):
+    ROOT.gSystem.Load(os.path.join(supera.get_lib_dir(),'libsupera.dylib'))
+else: raise Exception("libsupera.so/dylib are both missing")
 
 # Now load edep2supera library
 lib_path = os.path.join(os.path.dirname(__file__),"../../../")
 inc_path = os.path.join(os.path.dirname(__file__),"../../../../include/")
-ROOT.gSystem.Load(os.path.join(lib_path,'libedep2supera.so'))
+
+
+if os.path.isfile(os.path.join(lib_path,'libedep2supera.so')):
+    ROOT.gSystem.Load(os.path.join(lib_path,'libedep2supera.so'))
+elif os.path.isfile(os.path.join(lib_path,'libedep2supera.dylib')):
+    ROOT.gSystem.Load(os.path.join(lib_path,'libedep2supera.dylib'))
+else: raise Exception("libsupera.so/dylib are both missing")
 
 from ROOT import edep2supera
 from ROOT import supera
--- src/edep2supera/SuperaDriver.cxx
+++ src/edep2supera/SuperaDriver.cxx
@@ -210,7 +210,10 @@ namespace edep2supera {
 		result.pdg = edepsim_part.GetPDGCode();	 ///< PDG code
 		result.px = edepsim_part.GetInitialMomentum().Px();
 		result.py = edepsim_part.GetInitialMomentum().Py();
-		result.pz = edepsim_part.GetInitialMomentum().Pz();										///< (x,y,z) component of particle's initial momentum
+		result.pz = edepsim_part.GetInitialMomentum().Pz();				///< (x,y,z) component of particle's initial momentum
+		result.px_final = edepsim_part.Points.back().GetMomentum().X();
+		result.py_final = edepsim_part.Points.back().GetMomentum().Y();
+		result.pz_final = edepsim_part.Points.back().GetMomentum().Z();							  
 		result.vtx = supera::Vertex(start.X() / 10., start.Y() / 10., start.Z() / 10., start.T()); ///< (x,y,z,t) of particle's vertex information
 		result.end_pt = supera::Vertex(end.X() / 10., end.Y() / 10., end.Z() / 10., end.T());		///< (x,y,z,t) at which particle disappeared from G4WorldVolume
 		//result.process = edepsim_part.Points[0].GetProcess(); 										///< string identifier of the particle's creation process from Geant4
@@ -321,6 +324,11 @@ namespace edep2supera {
 		auto pdg_code    = supera_part.pdg;
 		auto g4type_main = edepsim_part.Points.front().GetProcess();
 		auto g4type_sub  = edepsim_part.Points.front().GetSubprocess();
+
+		if (g4type_main>10000||g4type_sub>10000)
+		{
+			LOG_FATAL() << "UNEXPECTED Process/Subprocess: "<<g4type_main<<"::"<<g4type_sub<< std::endl;
+		}
 
 		std::stringstream ss;
 		ss << (int)(g4type_main) << "::" << (int)(g4type_sub);
