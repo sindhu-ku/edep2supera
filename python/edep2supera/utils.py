f4f30d2fee2a86c4e526f7ad1bf9376c222751d8
sindhu-ku <s.kumaran@uci.edu>
2024-04-16 18:16:18 -0700

    add primary field to particle

--- python/edep2supera/utils.py
+++ python/edep2supera/utils.py
@@ -60,9 +60,8 @@ def larcv_particle(p):
     
     # particle's info setter
     larp.track_id         (p.part.trackid)
-    if hasattr(p.part, "genid") and hasattr(larp, "gen_id"):
-        larp.gen_id(p.part.genid)
-        
+    larp.gen_id           (p.part.genid)
+    larp.primary          (p.part.primary)    
     larp.pdg_code         (p.part.pdg)
     larp.momentum         (p.part.px,p.part.py,p.part.pz)
     larp.end_momentum     (p.part.end_px,p.part.end_py,p.part.end_pz)
